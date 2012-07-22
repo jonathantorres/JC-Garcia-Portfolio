1df66d8dd17008648758443bf06cf9f1cb6c061c
Jonathan Torres <jonathantorres41@gmail.com>
2012-07-22 17:27:02 -0400

    Setting up main loop

--- wp-content/themes/jcgarcia/index.php
+++ wp-content/themes/jcgarcia/index.php
@@ -1,172 +1,59 @@
 	  		<?php get_header(); ?>
 	  		
-	  		<ul id="all_posts">
+	  		<?php 
+		  		$all_posts = array();
+		  		$has_posts = true;
+		  		
+		  		//Get WP Posts
+		  		if (have_posts()) : while (have_posts()) : the_post();
+		  		
+		  			//The first category of the post
+		  			$categories = get_the_category(get_the_id());
+		  			$category = $categories[0]->cat_name;
+		  				
+		  			$all_posts[(get_the_time('U'))]['id'] = get_the_id();
+		  			$all_posts[(get_the_time('U'))]['title'] = get_the_title();
+		  			
+		  			//Need to make a switch for everything else : instagram, flickr etc ect
+		  			if ($category == 'Portfolio') {
+			  			$all_posts[(get_the_time('U'))]['type'] = 'portfolio';
+			  			$all_posts[(get_the_time('U'))]['catname'] = 'portfolio';
+		  			} else {
+			  			$all_posts[(get_the_time('U'))]['type'] = 'wp';
+			  			$all_posts[(get_the_time('U'))]['catname'] = 'post';
+		  			}
+		  			
+		  			$all_posts[(get_the_time('U'))]['permalink'] = get_permalink();
+		  			$all_posts[(get_the_time('U'))]['thumb'] = '/img/post_image.jpg';
+		  			$all_posts[(get_the_time('U'))]['excerpt'] = get_the_excerpt();
+		  			$all_posts[(get_the_time('U'))]['date'] = get_the_time('F, j, Y');
+		  			
+		  		endwhile; else:
+		  			$has_posts = false;
+		  		endif;
+		  		
+	  		?>
 	  		
-	  			<!-- WP Post -->
-	  			<li class="square wp">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img wp"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-	  						<img src="<?php bloginfo('template_url'); ?>/img/post_image.jpg" alt="Post">
-	  					</div>
-					</a>
-	  				<h3><a href="#">Essentials: Lincoln</a></h3>
-	  				<p>Once a rising star on the surf circuit, Lincoln Eather now makes his living developing web-based brands and building...</p>
-	  				<span class="post_type wp">post</span>
-	  			</li>
-	  			
-	  			<!-- Instagram Post -->
-	  			<li class="square instagram">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img instagram"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/instagram_image.jpg" alt="Post">
-						</div>
-					</a>
-	  				<h3><a href="#">Agriculture</a></h3>
-	  				<span class="post_type instagram">instagram</span>
-	  			</li>
-	  			
-	  			<!-- Flickr Post -->
-	  			<li class="square flickr">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img flickr"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/flickr_image.jpg" alt="Post">
-						</div>
-	  				</a>
-	  				<h3><a href="#">Fiddler of San Juan</a></h3>
-	  				<span class="post_type flickr">flickr</span>
-	  			</li>
-	  			
-	  			<!-- Twitter Post -->
-	  			<li class="square twitter">
-	  				<p>Todo un año de buena experiencia, arte,lifestyle, eventos, música. Toda la <a href="#">@BuenaVibraPR</a> mostrada en video <a href="#">vimeo.com/36554938</a> <a href="#">#BVPR</a></p>
-	  				<h3><a href="#">Tweet</a></h3>
-	  				<span class="post_type twitter">twitter</span>
-	  			</li>
-	  			
-	  			<!-- Portfolio Post -->
-	  			<li class="square portfolio">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img portfolio"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-		  				<div class="img_holder">
-		  					<img src="<?php bloginfo('template_url'); ?>/img/portfolio_image.jpg" alt="Post">
-		  				</div>
-	  				</a>
-	  				
-	  				<h3><a href="#">Kisses for the Lens</a></h3>
-	  				<span class="post_type portfolio">portfolio</span>
-	  			</li>
-	  			
-	  			<!-- Instagram Post -->
-	  			<li class="square instagram">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img instagram"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/instagram_image.jpg" alt="Post">
-						</div>
-	  				</a>
-	  				
-	  				<h3><a href="#">Agriculture</a></h3>
-	  				<span class="post_type instagram">instagram</span>
-	  			</li>
-	  			
-	  			<!-- Portfolio Post -->
-	  			<li class="square portfolio">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img portfolio"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-		  				<div class="img_holder">
-		  					<img src="<?php bloginfo('template_url'); ?>/img/portfolio_image.jpg" alt="Post">
-		  				</div>
-	  				</a>
-	  				
-	  				<h3><a href="#">Kisses for the Lens</a></h3>
-	  				<span class="post_type portfolio">portfolio</span>
-	  			</li>
-	  			
-	  			<!-- Twitter Post -->
-	  			<li class="square twitter">
-	  				<p>Todo un año de buena experiencia, arte,lifestyle, eventos, música. Toda la <a href="#">@BuenaVibraPR</a> mostrada en video <a href="#">vimeo.com/36554938</a> <a href="#">#BVPR</a></p>
-	  				<h3><a href="#">Tweet</a></h3>
-	  				<span class="post_type twitter">twitter</span>
-	  			</li>
-	  			
-	  			<!-- Instagram Post -->
-	  			<li class="square instagram">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img instagram"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/instagram_image.jpg" alt="Post">
-						</div>
-					</a>
-	  				<h3><a href="#">Agriculture</a></h3>
-	  				<span class="post_type instagram">instagram</span>
-	  			</li>
-	  			
-	  			<!-- Portfolio Post -->
-	  			<li class="square portfolio">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img portfolio"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-		  				<div class="img_holder">
-		  					<img src="<?php bloginfo('template_url'); ?>/img/portfolio_image.jpg" alt="Post">
-		  				</div>
-	  				</a>
-	  				
-	  				<h3><a href="#">Kisses for the Lens</a></h3>
-	  				<span class="post_type portfolio">portfolio</span>
-	  			</li>
-	  			
-	  			<!-- Twitter Post -->
-	  			<li class="square twitter">
-	  				<p>Todo un año de buena experiencia, arte,lifestyle, eventos, música. Toda la <a href="#">@BuenaVibraPR</a> mostrada en video <a href="#">vimeo.com/36554938</a> <a href="#">#BVPR</a></p>
-	  				<h3><a href="#">Tweet</a></h3>
-	  				<span class="post_type twitter">twitter</span>
-	  			</li>
-	  			
-	  			<!-- Flickr Post -->
-	  			<li class="square flickr">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img flickr"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/flickr_image.jpg" alt="Post">
-						</div>
-	  				</a>
-	  				<h3><a href="#">Fiddler of San Juan</a></h3>
-	  				<span class="post_type flickr">flickr</span>
-	  			</li>
+	  		<ul id="all_posts">
+		  		<?php if ($has_posts) : foreach($all_posts as $post => $the_post) :  ?>
+		  			<li class="square <?php echo $the_post['type']; ?>">
+		  				<a href="<?php echo $the_post['permalink']; ?>">
+			  				<div class="rollover">
+								<span class="over_img <?php echo $the_post['type']; ?>"></span>
+								<p class="post_date"><?php echo $the_post['date']; ?></p>
+							</div>
+							
+							<div class="img_holder">
+		  						<img src="<?php bloginfo('template_url'); echo $the_post['thumb']; ?>" alt="Post">
+		  					</div>
+						</a>
+		  				<h3><a href="<?php echo $the_post['permalink']; ?>"><?php echo $the_post['title']; ?></a></h3>
+		  				<p><?php echo $the_post['excerpt']; ?></p>
+		  				<span class="post_type <?php echo $the_post['type']; ?>"><?php echo $the_post['catname']; ?></span>
+		  			</li>
+		  		<?php endforeach; else: ?>
+	  				<p>No posts to display</p>
+	  			<?php endif; ?>
 	  		</ul><!-- #all_posts -->
 	  		
 	  		<?php get_footer(); ?>
