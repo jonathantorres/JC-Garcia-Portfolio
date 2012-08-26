2835e13b9028481aec077dcab55e5ac5686a7433
Jonathan Torres <jonathantorres41@gmail.com>
2012-08-25 21:50:53 -0400

    Added single page template.

---
+++ wp-content/themes/jcgarcia/page.php
@@ -0,0 +1,13 @@
+			<?php get_header(); ?>
+			
+	  		<div class="single">
+	  			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	  				<h1 class="post_title"><?php the_title(); ?></h1>
+		  			<div class="post_meta"></div>
+	  				<?php the_content(); ?>
+	  			<?php endwhile; endif; ?>
+	  		</div><!-- .single -->
+	  		
+	  		<?php get_sidebar(); ?>
+	  		
+	  		<?php get_footer(); ?>
