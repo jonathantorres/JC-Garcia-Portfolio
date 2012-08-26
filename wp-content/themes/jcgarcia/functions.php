187e99aab63118676eef06892d55ac428a51bb3d
Jonathan Torres <jonathantorres41@gmail.com>
2012-08-25 20:42:04 -0400

    Support for featured images, excerpts etc etc

---
+++ wp-content/themes/jcgarcia/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Control excerpt length */
+function control_excerpt_length($length) {
+	return 15;
+}
+add_filter('excerpt_length', 'control_excerpt_length', 999);
+
+/* Remove [...] from excerpt */
+function change_excerpt_end($text) { 
+	return str_replace('[...]', '...', $text); 
+} 
+add_filter('the_excerpt', 'change_excerpt_end');
+
+/* Featured Images */
+if (function_exists('add_theme_support')) { 
+	add_theme_support('post-thumbnails');
+
+	//additional image sizes
+	add_image_size('post-thumb', 201, 143);
+}
+
+?>
