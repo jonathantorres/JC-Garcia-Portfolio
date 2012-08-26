a4a0b88531c3c978140cc480ff397a537ce5eb43
Jonathan Torres <jonathantorres41@gmail.com>
2012-08-25 21:49:48 -0400

    Single page changes

--- wp-content/themes/jcgarcia/single.php
+++ wp-content/themes/jcgarcia/single.php
@@ -1,18 +1,26 @@
 			<?php get_header(); ?>
 			
 	  		<div class="single">
-	  			<h1 class="post_title">Essentials: Lincoln Eather</h1>
-	  			<div class="post_meta">
-	  				<span class="is_post">Post</span>
-	  				<span class="post_category"><span>Categoria:</span> Estilo</span>
-	  				<span class="post_comments"><span>Comentarios:</span>  10</span>
-	  			</div>
-	  			<img alt="Post Image" src="<?php bloginfo('template_url'); ?>/img/post_image_dummy.jpg">
-	  			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet gravida sem. Maecenas malesuada malesuada tempor. Maecenas quis ultrices purus. Nunc viverra egestas venenatis. Suspendisse potenti. Quisque sit amet massa quam. Aenean varius varius magna, a auctor libero vulputate malesuada. Nunc adipiscing, turpis in tempor congue, lorem eros ultrices lectus, eget porta sapien lacus quis augue. Duis sed sem velit, at <a href="#">sollicitudin</a> nibh. Etiam sodales dictum tempus.</p>
-
-				<p>Nullam a sem ac eros sagittis feugiat eget ut purus. Etiam vel erat mi, et porta nulla. Duis vulputate, urna lobortis consequat tempus, urna risus consectetur elit, id condimentum lectus diam et risus. Donec rutrum rhoncus tempus. Sed tristique imperdiet convallis. Nulla tempor est non nisi interdum porttitor. Pellentesque ut nisi et augue scelerisque sagittis. Sed fringilla, tellus eu pharetra malesuada, justo arcu vehicula mauris, a volutpat ante ipsum ut lectus.</p>
-
-				<p>Ut libero turpis, sodales ut suscipit ac, mattis at orci. Maecenas justo justo, aliquam porttitor sodales id, fermentum vitae lectus. Quisque euismod odio ut felis consequat elementum. Suspendisse vestibulum, dui nec gravida mollis, elit turpis viverra libero, et cursus justo ipsum sit amet odio. Mauris vel quam non tellus blandit tincidunt eu sit amet orci. Vivamus ac sapien non eros volutpat luctus quis sed ligula. Donec a ipsum ipsum. Sed vel nulla at ipsum lacinia aliquet. Vestibulum quis nisi est, ut dictum purus. Praesent lobortis libero et dolor convallis pretium. Vivamus ut enim elit. Duis nec tortor nec dui lobortis hendrerit sit amet vel mi. Nulla hendrerit dictum quam ac posuere. Integer nibh massa, egestas sed gravida eu, venenatis ac velit.</p>
+	  			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	  				<?php
+  						//The first category of the post
+  						$categories = get_the_category(get_the_id());
+  						$category = $categories[0]->cat_name;
+  					?>
+	  				<h1 class="post_title"><?php the_title(); ?></h1>
+		  			<div class="post_meta">
+		  				<span class="is_post">
+			  				<?php if ($category == 'Portfolio') : ?>
+			  					Portfolio
+							<?php else : ?>
+	  							Post
+	  						<?php endif; ?>
+		  				</span>
+		  				<span class="post_category"><span>Categoria:</span> <?php echo $category; ?> </span>
+		  				<span class="post_comments"><span>Comentarios:</span> <?php comments_number('0', '1', '%'); ?></span>
+		  			</div>
+	  				<?php the_content(); ?>
+	  			<?php endwhile; endif; ?>
 				
 				<div class="social_sharing">
 					<div class="twitter_btns">
