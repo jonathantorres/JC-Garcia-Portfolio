df3eb4eed66c6e364b19a8dc5b051f854f83b317
Jonathan Torres <jonathantorres41@gmail.com>
2012-09-03 16:31:18 -0400

    Changes on resume page

--- wp-content/themes/jcgarcia/resume.php
+++ wp-content/themes/jcgarcia/resume.php
@@ -14,7 +14,7 @@
 						<p class="knowledge">Poseo conocimientos en Social Media, Blogs, Medios Digitales, UX y amplia experiencia en la públición y administración de contenido en las plataforma Wordpress y Joomla.</p>
 						<h5 class="birth_title">Naci | Born</h5>
 						<p class="birth_date">17 de Marzo 1985 | Mar 17th, 1985</p>
-						<a href="#" class="dark_button linkedin">Linkedin</a>
+						<a href="http://linkedin.com/in/jcangustia/es" target="_blank" class="dark_button linkedin">Linkedin</a>
 						<a href="#" class="dark_button contacto">Contacto</a>
 					</div>
 				</div><!-- .profile -->
@@ -165,25 +165,33 @@
 							<span class="arrow right"></span>
 						</div><!-- .nav -->
 					</div><!-- heading -->
-					<div class="jobs_container">
+					<div class="jobs_container recomended">
 						<div class="jobs">
 							<div class="job full">
 								<img src="<?php bloginfo('template_url'); ?>/img/isis.jpg" class="pic_border">
-								<h2>Isis Zeledon - <span>Interactive Designer / Consultant at Nobox</span></h2>
-								<p class="quote">“Juan Carlos is a hard worker and very talented in the fast-paced online world. We work together on several Interactive projects and he's a genuine team player. I recommend him for whatever creative venture he pursues”.</p>
+								<h2>Isis Zeledon - <span>Interactive Designer / Consultant at Nobox Marketing Group</span></h2>
+								<p class="quote">“Juan Carlos is a hard worker and very talented in the fast-paced online world. We work together on several Interactive projects and he's a genuine team player. I recommend him for whatever creative venture he pursues.”</p>
 								<p class="dates">Abril 12, 2011.</p>
 							</div>
 							<div class="job full">
-								<img src="<?php bloginfo('template_url'); ?>/img/isis.jpg" class="pic_border">
-								<h2>Isis Zeledon - <span>Interactive Designer / Consultant at Nobox</span></h2>
-								<p class="quote">“Juan Carlos is a hard worker and very talented in the fast-paced online world. We work together on several Interactive projects and he's a genuine team player. I recommend him for whatever creative venture he pursues”.</p>
-								<p class="dates">Abril 12, 2011.</p>
+								<img src="<?php bloginfo('template_url'); ?>/img/jorge.jpg" class="pic_border">
+								<h2>Jorge Luis Suárez Rodríguez - <span>User Experience Director at Nobox Marketing Group</span></h2>
+							  <p class="quote">"Juan is tremendously reliable and creative designer. His broad skill set in design, UX, social media management and art direction, make him an excellent fit in any team. A very humble person, really responsible, super organized and resourceful. He is truly one of the key pieces of my creative team."</p>
+								<p class="dates">January 12, 2012.</p>
 							</div>
 							<div class="job full">
-								<img src="<?php bloginfo('template_url'); ?>/img/isis.jpg" class="pic_border">
-								<h2>Isis Zeledon - <span>Interactive Designer / Consultant at Nobox</span></h2>
-								<p class="quote">“Juan Carlos is a hard worker and very talented in the fast-paced online world. We work together on several Interactive projects and he's a genuine team player. I recommend him for whatever creative venture he pursues”.</p>
-								<p class="dates">Abril 12, 2011.</p>
+								<img src="<?php bloginfo('template_url'); ?>/img/valdivia.jpg" class="pic_border">
+						    <h2>Augusto Valdivia Tavárez - <span>Director / Editor at Revista Planeta Digital</span></h2>
+								<p class="quote">"Como revista de la Imagen Digital, Planeta Digital nació promoviendo la Web 2.0 y encontramos en Juan Carlos la persona que motorizó las mejores iniciativas en favor de la Web 2.0 del proyecto, localizando las aplicaciones más novedosas y atractivas para utilizarlas en el Blog, luego en Facebook y más tarde en Twittter, asi como en la búsqueda de contenido de vanguardia para el contenido de la revista."</p>
+                                
+                                
+                                <p class="dates">January 12, 2012.</p>
+							</div>
+							<div class="job full">
+								<img src="<?php bloginfo('template_url'); ?>/img/joan.jpg" class="pic_border">
+					      <h2>Joan Guerrero - <span>Social Media Associate at Numericit</span></h2>
+								<p class="quote">"Juan es un diseñador con valor agregado. Optimiza a niveles superior a los estándares, cualquier espacio online con poder visual y usabilidad. Tiene calidad que supera las tendencias en Dominicana y sabe cuáles son las que se mueven, en una web que cada día cambia más."</p>
+								<p class="dates">November 30, 2011.</p>
 							</div>
 						</div><!-- .jobs -->
 					</div><!-- .jobs_container -->
