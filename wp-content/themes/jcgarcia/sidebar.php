b8a66279afdbfb60d793554c90f837408bfd0c4e
Jonathan Torres <jonathantorres41@gmail.com>
2012-09-08 23:43:45 -0400

    Now the sidebar is showing posts

--- wp-content/themes/jcgarcia/sidebar.php
+++ wp-content/themes/jcgarcia/sidebar.php
@@ -1,80 +1,8 @@
 			<div id="all_posts" class="sidebar">
-	  			<!-- WP Post -->
-	  			<div class="square wp">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img wp"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-	  						<img src="<?php bloginfo('template_url'); ?>/img/post_image.jpg" alt="Post">
-	  					</div>
-					</a>
-	  				<h3><a href="#">Essentials: Lincoln</a></h3>
-	  				<p>Once a rising star on the surf circuit, Lincoln Eather now makes his living developing web-based brands and building...</p>
-	  				<span class="post_type wp">post</span>
-	  			</div>
-	  			
-	  			<!-- Instagram Post -->
-	  			<div class="square instagram">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img instagram"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/instagram_image.jpg" alt="Post">
-						</div>
-					</a>
-	  				<h3><a href="#">Agriculture</a></h3>
-	  				<span class="post_type instagram">instagram</span>
-	  			</div>
-	  			
-	  			<!-- Flickr Post -->
-	  			<div class="square flickr">
-	  				<a href="#">
-	  					<div class="rollover">
-							<span class="over_img flickr"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-						
-						<div class="img_holder">
-							<img src="<?php bloginfo('template_url'); ?>/img/flickr_image.jpg" alt="Post">
-						</div>
-	  				</a>
-	  				<h3><a href="#">Fiddler of San Juan</a></h3>
-	  				<span class="post_type flickr">flickr</span>
-	  			</div>
-	  			
-	  			<!-- Twitter Post -->
-	  			<div class="square twitter">
-	  				<p>Todo un año de buena experiencia, arte,lifestyle, eventos, música. Toda la <a href="#">@BuenaVibraPR</a> mostrada en video <a href="#">vimeo.com/36554938</a> <a href="#">#BVPR</a></p>
-	  				<h3><a href="#">Tweet</a></h3>
-	  				<span class="post_type twitter">twitter</span>
-	  			</div>
-	  			
-	  			<!-- Portfolio Post -->
-	  			<div class="square portfolio">
-	  				<a href="#">
-		  				<div class="rollover">
-							<span class="over_img portfolio"></span>
-							<p class="post_date">Abril, 10, 2012</p>
-						</div>
-		  				<div class="img_holder">
-		  					<img src="<?php bloginfo('template_url'); ?>/img/portfolio_image.jpg" alt="Post">
-		  				</div>
-	  				</a>
-	  				
-	  				<h3><a href="#">Kisses for the Lens</a></h3>
-	  				<span class="post_type portfolio">portfolio</span>
-	  			</div>
-	  			
-	  			<!-- Twitter Post -->
-	  			<div class="square twitter">
-	  				<p>Todo un año de buena experiencia, arte,lifestyle, eventos, música. Toda la <a href="#">@BuenaVibraPR</a> mostrada en video <a href="#">vimeo.com/36554938</a> <a href="#">#BVPR</a></p>
-	  				<h3><a href="#">Tweet</a></h3>
-	  				<span class="post_type twitter">twitter</span>
-	  			</div>
+	  			<?php $query = new WP_Query('posts_per_page=6&cat=-6'); ?>
+	  			<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+		  			<?php get_template_part('loop'); ?>
+		  		<?php endwhile; else: ?>
+	  				<p>No posts to display</p>
+	  			<?php endif; ?>
 	  		</div><!-- #all_posts -->
